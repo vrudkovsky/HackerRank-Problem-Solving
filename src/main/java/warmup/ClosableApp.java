82e712f9ee6e2a5cf087355fd967557c0045550b
Victor Rudkovsky <vpcomtek@gmail.com>
2022-02-21 10:01:06 +0200

    -- IntToBnrStr

---
+++ src/main/java/warmup/ClosableApp.java
@@ -0,0 +1,12 @@
+package warmup;
+
+public class ClosableApp {
+    public static void main(String[] args) {
+        MyClosable myClosable = new MyClosable();
+        System.out.println(myClosable.close());
+    }
+}
+
+alias j17="export JAVA_HOME=`/usr/libexec/java_home -v 17.0.1`; java -version"
+alias j11="export JAVA_HOME=`/usr/libexec/java_home -v 11.0.13`; java -version"
+alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8.0_311`; java -version"
